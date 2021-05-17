3fcef3b7585a6f27af8c6961efaa392a04af57ac
JeonginKim <mywnajsldkf@gmail.com>
2021-05-18 02:27:06 +0900

    feat : siginin xml

---
+++ pickt/app/src/main/java/com/example/pickt/SinginActivity.java
@@ -0,0 +1,14 @@
+package com.example.pickt;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SinginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singin);
+    }
+}
