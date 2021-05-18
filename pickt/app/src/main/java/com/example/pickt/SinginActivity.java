80ff5bac12e164ab619be88a5e0a3c01f6dbc185
JeonginKim <mywnajsldkf@gmail.com>
2021-05-19 01:34:39 +0900

    add : activity_signup.xml

--- pickt/app/src/main/java/com/example/pickt/SinginActivity.java
+++ pickt/app/src/main/java/com/example/pickt/SinginActivity.java
@@ -1,6 +1,8 @@
 package com.example.pickt;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class SinginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singin);
+    }
+
+    public void signupButton(View view){
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
     }
 }
