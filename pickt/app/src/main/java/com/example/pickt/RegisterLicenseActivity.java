bf058dd790e086bc506d9f41ad0e2b57158c365f
JeonginKim <mywnajsldkf@gmail.com>
2021-05-19 03:18:59 +0900

    feat : popup button - 운전면허증 촬영 버튼 클릭 시 카메라와 갤러리 모두 접근 기능을 위해 팝업 버튼을 생성함

--- pickt/app/src/main/java/com/example/pickt/RegisterLicenseActivity.java
+++ pickt/app/src/main/java/com/example/pickt/RegisterLicenseActivity.java
@@ -1,6 +1,10 @@
 package com.example.pickt;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,27 @@ public class RegisterLicenseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_license);
+
+        findViewById(R.id.licenseButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                final PopupMenu popupMenu = new PopupMenu(getApplicationContext(), view);
+                getMenuInflater().inflate(R.menu.picturepopup, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if (item.getItemId() == R.id.camera){
+                            Toast.makeText(RegisterLicenseActivity.this, "카메라 클릭", Toast.LENGTH_SHORT).show();
+                        }
+                        else if(item.getItemId() == R.id.gallery){
+                            Toast.makeText(RegisterLicenseActivity.this, "갤러 클릭", Toast.LENGTH_SHORT).show();
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
     }
+
 }
