bb039462d0f1b6d82bf33af91972bc309bb6d7a1
mywnajsldkf <mywnajsldkf@gmail.com>
2021-06-01 22:16:00 +0900

    add : activity_register_license_result

---
+++ pickt/app/src/main/java/com/example/pickt/activity/RegisterLicenseResultActivity.java
@@ -0,0 +1,14 @@
+package com.example.pickt.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.pickt.R;
+import android.os.Bundle;
+
+public class RegisterLicenseResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_license_result);
+    }
+}
