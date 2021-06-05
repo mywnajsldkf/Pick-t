0599cb560dce22303af720f0b626a61b37ac2244
mywnajsldkf <mywnajsldkf@gmail.com>
2021-06-05 14:49:38 +0900

    add : description fragment

--- pickt/app/src/main/java/com/example/pickt/fragment/TrailerFacilityFragment.java
+++ pickt/app/src/main/java/com/example/pickt/fragment/TrailerFacilityFragment.java
@@ -8,15 +8,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.pickt.R;
+import com.example.pickt.activity.TrailerRegisterActivity;
 
 import java.util.ArrayList;
 
 
 public class TrailerFacilityFragment extends Fragment {
 
+    private Button nextButton;
+    private static final String[] facility_list = {"간이 침대","인덕션","주방","캠핑용품","화장실","해먹"};
+    boolean[] checked_facility = new boolean[]{false, false, false, false, false, false, false};
+    int i = 0;
 
     public TrailerFacilityFragment() {
         // Required empty public constructor
@@ -33,11 +39,20 @@ public class TrailerFacilityFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_trailer_facility, container, false);
 
+        nextButton = (Button)view.findViewById(R.id.FacilityNextButton);
+
         final ArrayList<String> items = new ArrayList<String>();
         final ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_multiple_choice, items);
 
         final ListView listView = (ListView)getActivity().findViewById(R.id.facilityList);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trailer_facility, container, false);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TrailerRegisterActivity)getActivity()).replaceFragment(TrailerDescriptionFragment.newInstance());
+            }
+        });
+        return view;
     }
 }
